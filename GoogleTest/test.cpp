e2ef6a0006eb54d0e9c0f233bc6502b46d96e6df
hj41790 <hj41790@naver.com>
2023-07-07 13:56:50 +0900

    implement app

--- GoogleTest/test.cpp
+++ GoogleTest/test.cpp
@@ -2,6 +2,7 @@
 #include <gmock/gmock.h>
 
 #include "../DeviceDriver/DeviceDriver.cpp"
+#include "../DeviceDriver/App.cpp"
 
 using namespace testing;
 
@@ -56,4 +57,55 @@ TEST(DeviceDrvierTest, WriteFailTest) {
 		.WillRepeatedly(Return('A'));
 
 	EXPECT_THROW(driver.write(0xdeadbeaf, 'A'), WriteFailException);
+}
+
+TEST(DeviceDrvierTest, AppReadPassTest)
+{
+	MockFlashMemory flashmemory;
+	DeviceDriver driver(&flashmemory);
+	App app(&driver);
+
+	EXPECT_CALL(flashmemory, read)
+		.WillRepeatedly(Return(1));
+
+	EXPECT_NO_THROW(app.ReadAndPrint(0x1000, 0x1004));
+}
+
+TEST(DeviceDrvierTest, AppReadFailTest)
+{
+	MockFlashMemory flashmemory;
+	DeviceDriver driver(&flashmemory);
+	App app(&driver);
+
+	EXPECT_CALL(flashmemory, read)
+		.WillOnce(Return(3))
+		.WillOnce(Return(3))
+		.WillOnce(Return(3))
+		.WillRepeatedly(Return(1));
+
+	EXPECT_THROW(app.ReadAndPrint(0x1000, 0x1004), ReadFailException);
+}
+
+TEST(DeviceDrvierTest, AppWritePassTest)
+{
+	MockFlashMemory flashmemory;
+	DeviceDriver driver(&flashmemory);
+	App app(&driver);
+
+	EXPECT_CALL(flashmemory, read)
+		.WillRepeatedly(Return(0xFF));
+
+	EXPECT_NO_THROW(app.WriteAll(5));
+}
+
+TEST(DeviceDrvierTest, AppWriteFailTest)
+{
+	MockFlashMemory flashmemory;
+	DeviceDriver driver(&flashmemory);
+	App app(&driver);
+
+	EXPECT_CALL(flashmemory, read)
+		.WillRepeatedly(Return(0));
+
+	EXPECT_THROW(app.WriteAll(5), WriteFailException);
 }
