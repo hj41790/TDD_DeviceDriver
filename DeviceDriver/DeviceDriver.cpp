971949a1e0dc90d5e7f25c802d722a350ee4d963
hj41790 <hj41790@naver.com>
2023-07-07 11:13:56 +0900

    implement read function

--- DeviceDriver/DeviceDriver.cpp
+++ DeviceDriver/DeviceDriver.cpp
@@ -1,12 +1,31 @@
 #include "DeviceDriver.h"
 
+#include <vector>
+#include <windows.h>
+
+using namespace std;
+
 DeviceDriver::DeviceDriver(FlashMemoryDevice *hardware) : m_hardware(hardware)
 {}
 
 int DeviceDriver::read(long address)
 {
-	// TODO: implement this method properly
-	return (int)(m_hardware->read(address));
+	vector<unsigned char> res;
+
+	for (int i = 0; i < 5; i++)
+	{
+		res.push_back(m_hardware->read(address));
+		Sleep(200);
+	}
+
+	unsigned char value = res[0];
+	for(unsigned char item : res)
+	{
+		if (value != item)
+			throw ReadFailException("Read Fail");
+	}
+
+	return (int)value;
 }
 
 void DeviceDriver::write(long address, int data)
--- GoogleTest/test.cpp
+++ GoogleTest/test.cpp
@@ -1,6 +1,37 @@
 #include <gtest/gtest.h>
 #include <gmock/gmock.h>
 
-TEST(TestCaseName, TestName) {
-  EXPECT_EQ(1, 1);
+#include "../DeviceDriver/DeviceDriver.cpp"
+
+using namespace testing;
+
+class MockFlashMemory : public FlashMemoryDevice
+{
+public:
+	MOCK_METHOD(unsigned char, read, (long address), (override));
+	MOCK_METHOD(void, write, (long address, unsigned char data), (override));
+};
+
+TEST(DeviceDrvierTest, ReadSucceedTest) {
+	MockFlashMemory flashmemory;
+	DeviceDriver driver(&flashmemory);
+
+	EXPECT_CALL(flashmemory, read)
+		.Times(5)
+		.WillRepeatedly(Return('A'));
+
+	EXPECT_EQ('A', driver.read(0xdeadbeaf));
+}
+
+TEST(DeviceDrvierTest, ReadFailTest) {
+	MockFlashMemory flashmemory;
+	DeviceDriver driver(&flashmemory);
+
+	EXPECT_CALL(flashmemory, read)
+		.Times(5)
+		.WillOnce(Return('A'))
+		.WillOnce(Return('A'))
+		.WillRepeatedly(Return('B'));
+
+	EXPECT_THROW(driver.read(0xdeadbeaf), ReadFailException);
 }
